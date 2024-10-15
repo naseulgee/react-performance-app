a4d9a03d069c339da02fe0299af6306cb0c28eda
naseulgee <thwndglgkwk@naver.com>
2024-10-16 01:01:56 +0900

    useCallback 을 사용한 성능 향상

--- src/components/B.js
+++ src/components/B.js
@@ -3,15 +3,25 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-/** NOTE: B 문제 발생: 재사용성과 렌더링 최적화를 위해서 컴포넌트를 분할하였으나, Message 업데이트 시 하위의 컴포넌트도 같이 재렌더링 됨 */
+/** NOTE: B 문제 발생
+ * 재사용성과 렌더링 최적화를 위해서 컴포넌트를 분할하였으나,
+ * 1. Message 업데이트 시 하위의 컴포넌트도 같이 재렌더링 됨
+ * 2. 부모 컴포넌트가 재 렌더링 될 때 마다 하위 컴포넌트에 전달한 함수가 계속 새로 전달됨 -> 하위 컴포넌트 신규 생성
+ */
 const B = ({ message, todos }) => {
+  console.log('B components is Rendering')
+  const testFn = () => {}
   return (
     <article className='w-full'>
-      <h1 className='text-3xl'>B Component</h1>
+      <Title testFn={testFn} />
       <p className='my-5'>{message}</p>
       <List todos={todos} />
     </article>
   )
+}
+const Title = () => {
+  console.log('B Title is Rendering')
+  return <h1 className='text-3xl'>B Component</h1>
 }
 const List = ({ todos }) => {
   return (
--- src/components/C.js
+++ src/components/C.js
@@ -1,19 +1,32 @@
 /** NOTE: B 컴포넌트에서 성능개선을 한 예시 */
 
-import React, { useMemo } from 'react'
+import React, { useMemo, useCallback } from 'react'
 import PropTypes from 'prop-types'
 
-/** NOTE: B 문제 발생: 재사용성과 렌더링 최적화를 위해서 컴포넌트를 분할하였으나, Message 업데이트 시 하위의 컴포넌트도 같이 재렌더링 됨 */
+/** NOTE: B 문제 발생
+ * 재사용성과 렌더링 최적화를 위해서 컴포넌트를 분할하였으나,
+ * 1. Message 업데이트 시 하위의 컴포넌트도 같이 재렌더링 됨
+ * 2. 부모 컴포넌트가 재 렌더링 될 때 마다 하위 컴포넌트에 전달한 함수가 계속 새로 전달됨 -> 하위 컴포넌트 신규 생성
+ */
 const C = ({ message, todos }) => {
+  console.log('C components is Rendering')
+  /** NOTE: C 문제 2 해결: useCallback 를 사용한 문제 해결
+   * - const 함수명 = useCallback(함수, [])
+   */
+  const testFn = useCallback(() => {}, [])
   return (
     <article className='w-full'>
-      <h1 className='text-3xl'>C Component</h1>
+      <Title testFn={testFn} />
       <p className='my-5'>{message}</p>
       <List todos={todos} />
     </article>
   )
 }
-/** NOTE: C 문제 해결: useMemo 를 사용한 문제 해결
+const Title = React.memo(function Title() {
+  console.log('C Title is Rendering')
+  return <h1 className='text-3xl'>C Component</h1>
+})
+/** NOTE: C 문제 1 해결: useMemo 를 사용한 문제 해결
  * - const 컴포넌트명 = React.memo(function 컴포넌트명(props) { 컴포넌트 }, 비교함수)
  *    - 비교함수: props 가 object, array 등의 타입일 경우 세 번째 인자로 넣으며, 매개변수로 prevProps, nextProps 를 받는다
  * - const 컴포넌트명 = (props) => { return useMemo(() => ( … ), [props.값1, ...]) }
